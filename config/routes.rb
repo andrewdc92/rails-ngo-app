de0e248101366a965506fe94709935dd4cf455da
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-10-14 13:47:18 -0700

    donor routes working, fully built out with blank template pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get '/donors', to: "donors#index", as: "donors"
+  get '/donors/new', to: "donors#new", as: "new_donor"
+  post '/donors', to: "donors#create"
+  get '/donors/:id', to: "donors#show", as: "donor"
+  get '/donors/:id/edit', to: "donors#edit", as: "edit_donor"
+  patch '/donors/:id', to: "donors#update"
+  delete '/donors/:id', to: "donors#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "ngos#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014161226) do
+ActiveRecord::Schema.define(version: 20161014190232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "donors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "ngos", force: :cascade do |t|
     t.string   "name"
