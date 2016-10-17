13ca0f72c94820982aac25aa1e2e48869cb95d68
wnamen <williamjosephnamen@gmail.com>
2016-10-17 15:27:37 -0700

    added admin

--- db/migrate/20161016020253_add_admin_to_donors.rb
+++ db/migrate/20161016020253_add_admin_to_donors.rb
@@ -1,5 +1,5 @@
 class AddAdminToDonors < ActiveRecord::Migration[5.0]
   def change
-    add_column :donors, :admin, :boolean, default: false;
+    add_column :donors, :admin, :boolean, default: false
   end
 end
