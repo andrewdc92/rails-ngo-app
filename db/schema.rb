64290bc765fe110b15f776dd76a35c41a3c04df9
Sherie Carter <sherie1.carter@gmail.com>
2016-10-17 11:02:06 -0700

    removes margin top from body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016195459) do
+ActiveRecord::Schema.define(version: 20161017004431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20161016195459) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
   create_table "ngo_donors", force: :cascade do |t|
