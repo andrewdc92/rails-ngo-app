8723ff2f991c88af840bb5a6604b686add356d6c
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-10-16 17:30:51 -0700

    ran migration, added donate url to ngo model, templates correctly on each show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014204917) do
+ActiveRecord::Schema.define(version: 20161016233546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20161014204917) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "donate"
   end
 
   add_foreign_key "ngo_donors", "donors"
